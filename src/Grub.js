072acd38733186d75e05a88199d11605f54e7454
Lina Lu <Lina@Yaqings-MacBook-Air.local>
2015-07-08 12:34:38 -0700

    finished subclasses for taxonomy

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,25 @@
-var Grub = function() {
-};
+// .
+// ├── Grub
+// │   └── Bee
+// │       ├── HoneyMakerBee
+// │       └── ForagerBee
+// │          └── RetiredForagerBee
 
+var Grub = function() {
+	this.age = 0;
+	this.color = "pink";
+	this.food = "jelly";
+
+}; //Grub function 
+
+Grub.prototype.eat = function() {
+	//eats something;
+} //Grub eat method
+
+// Grub
+
+// Create a Grub class, in pseudoclassical style, with:
+// an age property that is set to 0
+// a color property that is set to pink
+// a food property that is set to jelly
+// an eat method
